d44bf9b015e423e739dacffc285f6d83a77a1722
s3551210 <aaz369@gmail.com>
2017-03-26 22:26:44 +1100

    This is EGit commit

--- src/EGitClassDemo.java
+++ src/EGitClassDemo.java
@@ -4,7 +4,6 @@ public class EGitClassDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello EGit");
-
+		System.out.println("Hello Eclipse");
 	}
-
 }
